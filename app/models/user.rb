2babba141594d89166c1e9e7edde3b9001280afe
crappypapa <devsolaoluwa@gmail.com>
2021-05-05 11:58:00 +0100

    Add active record association to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :created_events, class_name: 'Event', foreign_key: 'creator_id', dependent: :destroy
+  has_many :attendances, dependent: :destroy
+  has_many :attended_events, through: :attendances, source: :event
 end
