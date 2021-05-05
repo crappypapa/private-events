205a2fde0b268f13ba4145d1a44bf9acf5390340
crappypapa <devsolaoluwa@gmail.com>
2021-05-05 10:31:03 +0100

    Add sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create 
+    user = User.find_by_name(params[:name])
+    if user
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Successful!"
+    else
+      flash.now.alert = "Name not found!"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to rot_path, notice: "Logged out!"
+  end
+end
