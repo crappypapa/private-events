37c8de2e4adaffd365d328d4a2553ab27cb20e42
crappypapa <devsolaoluwa@gmail.com>
2021-05-06 18:51:35 +0100

    Add event attendance helper

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,13 @@
 module EventsHelper
+  def event_attendance (event)
+    if event.attendees.empty? 
+     attend = '<li >This event has no attendees yet</li>'
+    else 
+    attend = ' '
+     event.attendees.each do |attendee|
+      attend += "<li> #{attendee.name} </li>"
+      end
+    end
+    attend.html_safe
+  end
 end
