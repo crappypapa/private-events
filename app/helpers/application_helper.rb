90b2b6650f75bbdfe975bdd50bc2c5f2907343a6
crappypapa <devsolaoluwa@gmail.com>
2021-05-07 05:43:44 +0100

    Move app navigation logic to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+ 
+  def app_nav
+    if current_user 
+      "<div>
+        #{link_to current_user.name, user_path(current_user), class: 'btn'}
+        #{link_to 'Log out', logout_path, class: 'btn'}
+      </div>".to_s.html_safe
+    else 
+      "<div>
+        #{link_to 'Sign In', login_path, class: 'btn'}
+        #{link_to 'Sign Up', signup_path, class: 'btn'} 
+      </div>".html_safe
+    end
+  end
+
 end
